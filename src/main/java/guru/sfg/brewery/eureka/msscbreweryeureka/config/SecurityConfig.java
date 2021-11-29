cd1bea34184232e072844fd537e15f315d6ab69b
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-11-29 12:33:24 +0530

    added eureka

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package guru.sfg.brewery.eureka.msscbreweryeureka.config;
 
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
  * Created by jt on 5/16/20.
  */
-//@Configuration
-public class SecurityConfig{} /*extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,5 +22,21 @@ public class SecurityConfig{} /*extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("netflix").password("eureka")
+                .authorities("ADMIN");
+    }
+
+
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //  http.csrf (). Disable(); // one way is to close CSRF directly, and the other is to configure URL to release
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }*/
+
 }
-*/
